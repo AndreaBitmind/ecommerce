728408c4be49a37d7c17cba6327c6f43fccf0f7d
Andrea Martinez <andrea.maort@gmail.com>
2023-07-25 13:43:26 +0200

    feat: filter system added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import Products from "./components/Products";
-import { products } from "./mocks/products.json";
+import { products as initialProducts } from "./mocks/products.json";
 
 function App() {
+  const [products] = useState(initialProducts);
+  const [filters, setFilters] = useState({ category: "all", minPrice: 0 });
+
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === "all" || product.category === filters.category)
+      );
+    });
+  };
+
+  const filteredProducts = filterProducts(products);
+
   return (
     <>
-      <h1>Shopping Cart</h1>
-      <Products products={products} />
+      <h1>Welcome to Andrea's eCommerce 🛒</h1>
+      <Products products={filteredProducts} />
     </>
   );
 }
