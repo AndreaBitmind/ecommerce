4cd6f40d883cb0d2aa1e58bb5db254fe92fa761e
Andrea Martinez <andrea.maort@gmail.com>
2023-07-27 16:56:46 +0200

    feat: reducer moved to another file

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -1,45 +1,10 @@
-import { createContext, useReducer, useState } from "react";
+import { createContext, useReducer } from "react";
+import { cartReducer, initialState } from "../reducers/cart";
 
 export const CartContext = createContext();
 
-const initialState = [];
-const reducer = (state, action) => {
-  const { type: actionType, payload: actionPayload } = action;
-
-  switch (actionType) {
-    case "ADD_TO_CART": {
-      const { id } = actionPayload;
-      const productInCartIndex = state.findIndex((item) => item.id === id);
-
-      if (productInCartIndex >= 0) {
-        const newState = structuredClone(state);
-        newState[productInCartIndex].quantity += 1;
-        return newState;
-      }
-
-      return [
-        ...state,
-        {
-          ...actionPayload, //product
-          quantity: 1,
-        },
-      ];
-    }
-
-    case "REMOVE_FROM_CART": {
-      const { id } = actionPayload;
-      return state.filter((item) => item.id !== id);
-    }
-
-    case "CLEAR_CART": {
-      return initialState;
-    }
-  }
-  return state;
-};
-
 export function CartProvider({ children }) {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(cartReducer, initialState);
 
   const addToCart = (product) =>
     dispatch({
