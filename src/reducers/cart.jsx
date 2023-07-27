b6aa16e0f6160e42127f364e57ded3aec7b729d0
Andrea Martinez <andrea.maort@gmail.com>
2023-07-27 17:23:46 +0200

    feat: added localstorage persistence

--- src/reducers/cart.jsx
+++ src/reducers/cart.jsx
@@ -1,4 +1,10 @@
-export const cartInitialState = [];
+export const cartInitialState =
+  JSON.parse(window.localStorage.getItem("cart")) || [];
+
+//update localStorage with state for cart
+export const updateLocalStorage = (state) => {
+  window.localStorage.setItem("cart", JSON.stringify(state));
+};
 
 export const cartReducer = (state, action) => {
   const { type: actionType, payload: actionPayload } = action;
@@ -11,24 +17,30 @@ export const cartReducer = (state, action) => {
       if (productInCartIndex >= 0) {
         const newState = structuredClone(state);
         newState[productInCartIndex].quantity += 1;
+        updateLocalStorage(newState);
         return newState;
       }
 
-      return [
+      const newState = [
         ...state,
         {
           ...actionPayload, //product
           quantity: 1,
         },
       ];
+      updateLocalStorage(newState);
+      return newState;
     }
 
     case "REMOVE_FROM_CART": {
       const { id } = actionPayload;
-      return state.filter((item) => item.id !== id);
+      const newState = state.filter((item) => item.id !== id);
+      updateLocalStorage(newState);
+      return newState;
     }
 
     case "CLEAR_CART": {
+      updateLocalStorage(cartInitialState);
       return cartInitialState;
     }
   }
