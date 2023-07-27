d74b3f5683f19a06bc9ffca25d782de8fff3eba7
Andrea Martinez <andrea.maort@gmail.com>
2023-07-27 16:07:59 +0200

    feat: removeFromCart funciont implemented

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,21 +1,38 @@
-import { AddToCartIcon } from "./icons";
+import { useCart } from "../hooks/useCart";
+import { AddToCartIcon, RemoveFromCartIcon } from "./icons";
 import "./Products.css";
 
 function Products({ products }) {
+  const { addToCart, cart, removeFromCart } = useCart();
+
+  const checkProductInCart = (product) => {
+    return cart.some((item) => item.id === product.id);
+  };
+
   return (
     <main className="products">
       <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <img src={product.thumbnail} alt={product.title} />
-            <div>
-              <strong>{product.title}</strong> - €{product.price}
-            </div>
-            <button>
-              <AddToCartIcon />
-            </button>
-          </li>
-        ))}
+        {products.map((product) => {
+          const isProductInCart = checkProductInCart(product);
+          return (
+            <li key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+              <div>
+                <strong>{product.title}</strong> - €{product.price}
+              </div>
+              <button
+                style={{ backgroundColor: isProductInCart ? "red" : "#09f" }}
+                onClick={() => {
+                  isProductInCart
+                    ? removeFromCart(product)
+                    : addToCart(product);
+                }}
+              >
+                {isProductInCart ? <RemoveFromCartIcon /> : <AddToCartIcon />}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </main>
   );
